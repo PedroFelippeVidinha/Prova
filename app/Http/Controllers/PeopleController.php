557859a3f55eda5454eb4a09fbb1c2d81dbffdcc
Pedro Felippe <pedrofelippervidinha@gmail.com>
2023-03-02 00:36:36 -0300

    termino controller

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\people;
+use App\Models\People;
 use Illuminate\Http\Request;
 
 class PeopleController extends Controller
@@ -12,9 +12,24 @@ class PeopleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->all();
+        
+        $people = People::where(function($query) use($request){
+            if(isset($request->search)){
+                $query = $query->where('name', 'like', "%".$request->search."%");
+            }
+                        
+            if(isset($request->duration)){
+                $query = $query->where('duration', $request->duration);
+            }
+
+        })->paginate(50);
+          
+        return view ('people.index', compact('people', 'search'))
+            ->with('i', (request()->input('page', 1) - 1) * 50);
+
     }
 
     /**
@@ -24,7 +39,7 @@ class PeopleController extends Controller
      */
     public function create()
     {
-        //
+        return view ('people.create');
     }
 
     /**
@@ -35,51 +50,100 @@ class PeopleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'nullable|string|max:100',
+            'cpf' => 'nullable|string|max:20',
+            'email' => 'nullable|string|max:50',
+            'date_birth' => 'nullable|string|max:10',
+            'nationality' => 'nullable|string|max:20'
+        ]);
+        
+        $people = [
+            'name' => $request->name,
+            'cpf' => $request->cpf,
+            'email' => $request->email,
+            'date_birth' => $request->date_birth,
+            'nationality' => $request->nationality
+        ];
+        
+        People::create($people);
+
+        return redirect()->route('people')
+                   ->with('success','Pessoa criada com sucesso.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\people  $people
+     * @param  \App\Models\People  $people
      * @return \Illuminate\Http\Response
      */
-    public function show(people $people)
+    public function show($id)
     {
-        //
+        $people = People::where('id', $id)->first();
+
+        return view('people.show',compact('people'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\people  $people
+     * @param  \App\Models\People  $people
      * @return \Illuminate\Http\Response
      */
-    public function edit(people $people)
+    public function edit($id)
     {
-        //
+        $people = People::where('id', $id)->first();
+
+        return view('people.edit',compact('people'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\people  $people
+     * @param  \App\Models\People  $people
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, people $people)
     {
-        //
+        $cadastro = $request->all();
+        $id = $cadastro['id'];
+        
+        $request->validate([
+            'name' => 'nullable|string|max:100',
+            'cpf' => 'nullable|string|max:20',
+            'email' => 'nullable|string|max:50',
+            'date_birth' => 'nullable|string|max:10',
+            'nationality' => 'nullable|string|max:20'
+        ]);
+
+        $newPeople = People::find($id);
+
+        $newPeople->name = $request->name;
+        $newPeople->cpf = $request->cpf;
+        $newPeople->email = $request->email;
+        $newPeople->date_birth = $request->date_birth;
+        $newPeople->nationality = $request->nationality;
+
+        $newPeople->update($newPeople->toArray());
+
+        return redirect()->route('people')
+                        ->with('success','Pessoa atualizada com sucesso.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\people  $people
+     * @param  \App\Models\People  $people
      * @return \Illuminate\Http\Response
      */
-    public function destroy(people $people)
+    public function destroy($id)
     {
-        //
+        $people = People::find($id);
+        $people->delete();
+
+        return redirect()->route('people')
+                        ->with('success','Pessoa excluída com sucesso.');
     }
 }
